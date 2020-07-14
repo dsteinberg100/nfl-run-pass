f66a4a96a5e9c52f9cd80867df366054ee4a1f9f
richjand <richjand@umich.edu>
2020-07-13 17:59:37 -0700

    added workflow to initial-xgb

--- initial-xgb.R
+++ initial-xgb.R
@@ -9,13 +9,28 @@ library(purrr)
 dat <- readRDS('nfl-pbp.RDS') %>%
   tibble()
 
-traindat <- filter(dat, season >= 2014 & 
+dat_train <- filter(dat, season >= 2014 & 
                      season_type == 'REG' & 
                      down %in% c(1,2,3) &
                      !is.na(qb_dropback) &
                      !is.na(score_differential))
 
-#home_wp_post
-#yd
+qb_recipe <- recipe(qb_dropback ~ down + 
+                      ydstogo + 
+                      yardline_100 + 
+                      score_differential + 
+                      qtr + 
+                      half_seconds_remaining,
+    data = dat_train)
 
-variables <- c('score_differential','qtr',)
+qb_model <- 
+  boost_tree(
+    trees = 1000, 
+  ) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
+
+qb_workflow <- workflow() %>%
+  add_recipe(qb_recipe) %>%
+  add_model(qb_model)
+
